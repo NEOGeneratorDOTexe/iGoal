415e1271136fe1b2a6649385001869be35ce9b25
bwauu <benjamin.williamsappelberg.0315@student.uu.se>
2022-06-01 13:28:05 +0200

    main.js: added another alert to declare that the styling is done + a to-do list for implementing desired functionality of application.
    main.css: done.
    
    index.html: added a comment above our action that will be

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert("iGoal currently under production... Behold...")
+alert("App style is done!")
+alert("iGoal's instructions currently under production... ")
+/* TODO: Requirement fields:
+*   Userstory:
+*   1. Add a goal
+*       a) User input Text of desired goal.
+*       b) User clicks on the "Add goal" submit-button.
+*       c) result: Goal gets added to the list on the front-end.
+*   2. Edit a goal
+*     a) User clicks on the "Edit" button on a previously added task in the list of goals.
+*     b) User change text via text input (readonly off)
+*     c) User press "Accept Change"
+*     d) result: The goal that the user desired to edit was change accordingly to the text input.
+*   3. Delete a goal -- same as edit a goal basically but deletes the goal rather than change it
+*      result: the goal that the user desired to delete was deleted and does not exists in tasks
+*
+*
+* */
